c003e0c88a0b0e49914b6b3251fde985ec3b1693
wei-huan <1520799054@qq.com>
2022-11-13 22:00:10 +0800

    add some basic block entrance check

---
+++ src/middle/BasicBlock.cpp
@@ -0,0 +1,32 @@
+#include "BasicBlock.hpp"
+
+void BasicBlock::push_ir_instr(IRInstr *x) {
+  // 检查基本块是否已经有出口了
+  // if (x->oper == IRInstr::BRANCH || x->oper == IRInstr::RET) {
+  //   if (have_exit == true) {
+  //     throw RuntimeError("basic block " + std::to_string(label) +
+  //                        " already have exit");
+  //   } else {
+  //     have_exit = true;
+  //   }
+  // }
+  instrs.emplace_back(x);
+}
+
+void BasicBlock::push_prev(int prev_label) {
+  // //! check if there is already exists, avoid redundance
+  if (!count(prev_bb.begin(), prev_bb.end(), prev_label)) {
+    throw RuntimeError("basic block " + std::to_string(label) +
+                       " already have previous basic block " +
+                       std::to_string(prev_label));
+  } else {
+    prev_bb.emplace_back(prev_label);
+  }
+}
+
+void BasicBlock::print_ir_code() {
+  for (auto const &i : instrs) {
+    auto str = i->gen_code();
+    std::cout << "  " << str << std::endl;
+  }
+}
--- src/middle/BasicBlock.hpp
+++ src/middle/BasicBlock.hpp
@@ -1,38 +1,30 @@
 /// Basic Block Description
 #pragma once
 
+#include <algorithm>
 #include <list>
 #include <memory>
 #include <string>
 #include <vector>
-#include <algorithm>
 
+#include "common/errors.hpp"
 #include "middle/IR.hpp"
 
+using std::count;
 using std::list;
 using std::string;
 using std::unique_ptr;
 using std::vector;
-using std::count;
 
 // evey function may have many basic blocks
 // basic blocks have only one entry and one entrance
 struct BasicBlock {
-    int label;            // label = 0 for entry basic block
-    vector<int> prev_bb;  // info about control flow
-    list<unique_ptr<IRInstr>> instrs;
-    BasicBlock(int label) : label(label){};
-    void push_ir_instr(IRInstr* x) { instrs.emplace_back(x); }
-    void push_prev(int prev_label) {
-        //! check if there is already exists
-        if (!count(prev_bb.begin(), prev_bb.end(), prev_label)) {
-            prev_bb.emplace_back(prev_label);
-        }
-    }
-    void print_ir_code() {
-        for (auto const& i : instrs) {
-            auto str= i->gen_code();
-            std::cout << "  " << str << std::endl;
-        }
-    }
+  bool have_exit = false;
+  int label;           // label = 0 for entry basic block
+  vector<int> prev_bb; // info about control flow
+  list<unique_ptr<IRInstr>> instrs;
+  BasicBlock(int label) : label(label){};
+  void push_ir_instr(IRInstr *x);
+  void push_prev(int prev_label);
+  void print_ir_code();
 };
